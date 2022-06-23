4e30d73fd25aac44b81f29f3a8e6328e6d624503
magalie <magalie.waligora@gmail.com>
2022-06-23 15:22:07 +0200

    populaires

--- movieapp/src/app/populaires/populaires.page.ts
+++ movieapp/src/app/populaires/populaires.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-populaires',
   templateUrl: './populaires.page.html',
   styleUrls: ['./populaires.page.scss'],
 })
-export class PopulairesPage implements OnInit {
+export class PopulairesPage {
+  popularity: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=a4872679cc543a16475400119b475fb6')
+    .subscribe(response => {
+      console.log(response['results']);
+      this.popularity = response['results'];
+    });
   }
 
 }
