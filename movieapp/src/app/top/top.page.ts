356ad62c45b64a4843d505668072a6cc04260b51
magalie <magalie.waligora@gmail.com>
2022-06-23 17:34:32 +0200

    top debut

--- movieapp/src/app/top/top.page.ts
+++ movieapp/src/app/top/top.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.page.html',
   styleUrls: ['./top.page.scss'],
 })
-export class TopPage implements OnInit {
+export class TopPage {
+  top: any[] = [];
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=a4872679cc543a16475400119b475fb6')
+    .subscribe(response => {
+      console.log(response['results']);
+      this.top = response['results'];
+    });
   }
-
 }
