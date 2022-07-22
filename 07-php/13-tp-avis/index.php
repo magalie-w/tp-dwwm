88a409f9af3c332cb0cae82474947e2eab22445f
magalie <magalie.waligora@gmail.com>
2022-07-22 11:53:07 +0200

    html/tailwind

--- 07-php/13-tp-avis/index.php
+++ 07-php/13-tp-avis/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -15,12 +16,15 @@
         </header>
 
         <main>
+            <!-- PREMIER BLOC -->
             <div>
                 <h2 class="bg-gray-200 rounded-t p-2">Note moyenne</h2>
 
-                <div class="flex justify-around border items-center">
-                    <div class="text-center">
-                        <div class="text-amber-300">3.3/5</div>
+                <div class="flex justify-around border items-center pt-5 pb-5">
+
+                    <!-- GAUCHE -->
+                    <div class="text-center space-y-5">
+                        <div class="text-amber-300 font-bold">3.3/5</div>
 
                         <div class="flex">
                             <img src="img/star-regular.svg" width="20" alt="star avis" />
@@ -37,8 +41,9 @@
                         <div>4 avis</div>
                     </div>
 
+                    <!-- MILIEU -->
                     <div>
-                        <div class="flex items-baseline">
+                        <div class="flex items-baseline space-x-2">
                             <div class="flex">
                                 <span>5</span>
 
@@ -54,7 +59,7 @@
                             </div>
                         </div>
 
-                        <div class="flex items-baseline">
+                        <div class="flex items-baseline space-x-2">
                             <div class="flex">
                                 <span>4</span>
 
@@ -70,7 +75,7 @@
                             </div>
                         </div>
 
-                        <div class="flex items-baseline">
+                        <div class="flex items-baseline space-x-2">
                             <div class="flex">
                                 <span>3</span>
 
@@ -86,7 +91,7 @@
                             </div>
                         </div>
 
-                        <div class="flex items-baseline">
+                        <div class="flex items-baseline space-x-2">
                             <div class="flex">
                                 <span>2</span>
 
@@ -102,7 +107,7 @@
                             </div>
                         </div>
 
-                        <div class="flex items-baseline">
+                        <div class="flex items-baseline space-x-2">
                             <div class="flex">
                                 <span>1</span>
 
@@ -119,60 +124,132 @@
                         </div>
                     </div>
 
-                    <div class="text-center">
+                    <!-- DROITE -->
+                    <div class="text-center space-y-2">
                         <div>Laissez votre avis</div>
 
-                        <div class="bg-blue-500 text-white rounded">Noter</div>
+                        <div> 
+                            <button class="bg-blue-500 text-white rounded p-2">Noter</button>
+
+                        </div>
                     </div>
                 </div>
             </div>
 
+            <!-- DEUXIEME BLOC -->
             <div class="mt-5">
                 <h2 class="bg-gray-200 rounded-t p-2">Publier un avis</h2>
 
-                <div class="flex justify-around border items-center">
+                <div class="flex border justify-center pt-5 pb-5">
                     
-                    <form action="" method="post">
-                        <div>
-                            <label for="name">Nom</label>
-                            <input type="text" name="name" id="name" placeholder="Votre nom">
+                    <form action="" method="post" class="space-y-4 w-[500px]">
+                        <div class="flex w-full items-center">
+                            <label for="name" class="w-[145px]">Nom</label>
+                            <input type="text" name="name" id="name" placeholder="Votre nom" class="w-[500px]">
                         </div>
 
-                        <div>
+                        <div class="flex items-center space-x-3">
                             <label>Commentaire</label>
-                            <textarea rows="3" cols="30" name="review" id="review" placeholder="Votre commentaire"></textarea>
+                            <textarea rows="1" cols="30" name="review" id="review" placeholder="Votre commentaire" class="w-[500px]"></textarea>
                         </div>
 
                         <div class="flex">
-                            <legend>Note : </legend>
+                            <legend class="w-[150px]">Note</legend>
 
-                            <div>
-                                <input type="radio" id="note1" name="note" value="note1"
+                            <div class="w-full flex space-x-3">
+                                <div>
+                                    <input type="radio" id="note1" name="note" value="note1"
                                     checked>
-                                <label for="note1">1</label>
-                            </div>
+                                    <label for="note1">1</label>
+                                </div>
 
-                            <div>
-                                <input type="radio" id="note2" name="note" value="note2">
-                                <label for="note2">2</label>
-                            </div>
+                                <div>
+                                    <input type="radio" id="note2" name="note" value="note2">
+                                    <label for="note2">2</label>
+                                </div>
 
-                            <div>
-                                <input type="radio" id="note3" name="note" value="note3">
-                                <label for="note3">3</label>
-                            </div>
+                                <div>
+                                    <input type="radio" id="note3" name="note" value="note3">
+                                    <label for="note3">3</label>
+                                </div>
 
-                            <div>
-                                <input type="radio" id="note4" name="note" value="note4">
-                                <label for="note4">4</label>
-                            </div>
+                                <div>
+                                    <input type="radio" id="note4" name="note" value="note4">
+                                    <label for="note4">4</label>
+                                </div>
 
-                            <div>
-                                <input type="radio" id="note5" name="note" value="note5">
-                                <label for="note5">5</label>
+                                <div>
+                                    <input type="radio" id="note5" name="note" value="note5">
+                                    <label for="note5">5</label>
+                                </div>
                             </div>
                         </div>
+                        
+                        <div class="ml-[110px]">
+                            <button class="bg-blue-500 text-white rounded p-2">Noter</button>
+                        </div>
                     </form>
+                </div>
+            </div>
+
+            <!-- TROISIEME BLOC -->
+            <div>
+                <!-- AVIS -->
+                <div class="mt-5 flex space-x-5">
+                    <div>
+                        <img src="img/circle-solid.svg" width="60px" alt="avatar" />
+                    </div>
+
+                    <div class="w-full border">
+                        <div>
+                            <h2 class="bg-gray-200 rounded-t p-2">Fiorella Mota</h2>
+                        </div>
+
+                        <div class="p-2">
+                            <div class="flex">
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                            </div>
+
+                            <div>Très bon restaurant</div>
+                        </div>
+
+                        <div>
+                            <div class="bg-gray-200 rounded-b p-1 text-end">Mercredi 9 juillet 2022 à 11h43</div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- AVIS -->
+                <div class="mt-5 flex space-x-5">
+                    <div>
+                        <img src="img/circle-solid.svg" width="60px" alt="avatar" />
+                    </div>
+
+                    <div class="w-full border">
+                        <div>
+                            <h2 class="bg-gray-200 rounded-t p-2">Fiorella Mota</h2>
+                        </div>
+
+                        <div class="p-2">
+                            <div class="flex">
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                                <img src="img/star-regular.svg" width="20" alt="star avis" />
+                            </div>
+
+                            <div>Très bon restaurant</div>
+                        </div>
+
+                        <div>
+                            <div class="bg-gray-200 rounded-b p-1 text-end">Mercredi 9 juillet 2022 à 11h43</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
