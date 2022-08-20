d38c0612dddd82ff1edbe8db428f0300318ba5ec
magalie <magalie.waligora@gmail.com>
2022-08-20 17:25:41 +0200

    tp batterie js

--- 03-javascript/11-batterie/script.js
+++ 03-javascript/11-batterie/script.js
@@ -9,14 +9,18 @@ var snare = document.getElementById("snare");
 var tom = document.getElementById("tom");
 var tink = document.getElementById("tink");
 
+
+// AVEC LES TOUCHES
 window.addEventListener("keydown", function (event) {
 
     if (event.key == "q") {
         //Active l'audio lorsque que l'on appuie sur la touche q
+    setTimeout(function () {
         clap.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
-        document.getElementById("q");
+        // document.getElementById("q");
         q.classList.add("playing");
 
         //On supprime la class playing lorsque la touche est relachée
@@ -27,7 +31,9 @@ window.addEventListener("keydown", function (event) {
         
     } else if (event.key == "s") {
         //Active l'audio lorsque que l'on appuie sur la touche s
+    setTimeout(function () {
         hithat.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("s");
@@ -41,7 +47,9 @@ window.addEventListener("keydown", function (event) {
 
     } else if (event.key == "d") {
         //Active l'audio lorsque que l'on appuie sur la touche d
+    setTimeout(function () {
         kick.play();
+    }, 250);
         
         //Créer la class playing pour ajouter un effet css
         document.getElementById("d");
@@ -55,7 +63,9 @@ window.addEventListener("keydown", function (event) {
     
     } else if (event.key == "f") {
         //Active l'audio lorsque que l'on appuie sur la touche f
+    setTimeout(function () {
         openhat.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("f");
@@ -69,7 +79,9 @@ window.addEventListener("keydown", function (event) {
     
     } else if (event.key == "g") {
         //Active l'audio lorsque que l'on appuie sur la touche g
+    setTimeout(function () {
         boom.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("g");
@@ -84,7 +96,9 @@ window.addEventListener("keydown", function (event) {
     } else if (event.key == "h") {
 
         //Active l'audio lorsque que l'on appuie sur la touche h
+    setTimeout(function () {
         ride.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("h");
@@ -99,7 +113,9 @@ window.addEventListener("keydown", function (event) {
     } else if (event.key == "j") {
             
         //Active l'audio lorsque que l'on appuie sur la touche j
+    setTimeout(function () {
         snare.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("j");
@@ -113,7 +129,9 @@ window.addEventListener("keydown", function (event) {
     
     } else if (event.key == "k") {
         //Active l'audio lorsque que l'on appuie sur la touche k
+    setTimeout(function () {
         tom.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("k");
@@ -127,7 +145,9 @@ window.addEventListener("keydown", function (event) {
     
     } else if (event.key == "l") {
         //Active l'audio lorsque que l'on appuie sur la touche l
+    setTimeout(function () {
         tink.play();
+    }, 250);
 
         //Créer la class playing pour ajouter un effet css
         document.getElementById("l");
@@ -142,4 +162,131 @@ window.addEventListener("keydown", function (event) {
 });
 
 
+// AU CLIC
 
+// clap
+document.getElementById("q").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        clap.play();  
+    }, 250);
+    // ajout de la class au click
+    q.classList.add("playing");
+})
+
+document.getElementById("q").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    q.classList.remove("playing");
+})
+
+// hithat
+document.getElementById("s").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        hithat.play();
+    }, 250);
+    // ajout de la class au click
+    s.classList.add("playing");
+})
+
+document.getElementById("s").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    s.classList.remove("playing");
+})
+
+
+//  kick
+document.getElementById("d").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        kick.play();
+    }, 250);
+    // ajout de la class au click
+    d.classList.add("playing");
+})
+
+document.getElementById("d").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    d.classList.remove("playing");
+})
+
+// openhat
+document.getElementById("f").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        openhat.play();
+    }, 250);
+    // ajout de la class au click
+    f.classList.add("playing");
+})
+
+document.getElementById("f").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    f.classList.remove("playing");
+})
+
+// boom
+document.getElementById("g").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        boom.play();
+    }, 250);
+    // ajout de la class au click
+    g.classList.add("playing");
+})
+
+document.getElementById("g").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    g.classList.remove("playing");
+})
+
+// ride
+document.getElementById("h").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        ride.play();
+    }, 250);
+    // ajout de la class au click
+    h.classList.add("playing");
+})
+
+document.getElementById("h").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    h.classList.remove("playing");
+})
+
+// snare
+document.getElementById("j").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        snare.play();
+    }, 250);
+    // ajout de la class au click
+    j.classList.add("playing");
+})
+
+document.getElementById("j").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    j.classList.remove("playing");
+})
+
+// tom
+document.getElementById("k").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        tom.play();
+    }, 250);
+    // ajout de la class au click
+    k.classList.add("playing");
+})
+
+document.getElementById("k").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    k.classList.remove("playing");
+})
+
+// tink
+document.getElementById("l").addEventListener("mousedown", function(event) {
+    setTimeout(function () {
+        tink.play();
+    }, 250);
+    // ajout de la class au click
+    l.classList.add("playing");
+})
+
+document.getElementById("l").addEventListener("mouseup", function(event) {
+    // delete de la class au levé du click
+    l.classList.remove("playing");
+})
